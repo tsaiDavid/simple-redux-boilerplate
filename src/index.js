1363085990e8690e4d5cf181c36c838ceaec833a
Yilen <yilen.pan@gmail.com>
2016-01-29 17:46:48 -0800

    Added react-router-redux for client side routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { syncHistory } from 'react-router-redux';
+import { hashHistory } from 'react-router'
+
 /**
  * Both configureStore and Root are required conditionally.
  * See configureStore.js and Root.js for more details.
@@ -7,9 +11,16 @@ import ReactDOM from 'react-dom';
 import { configureStore } from './store/configureStore';
 import { Root } from './containers/Root';
 
+
 const store = configureStore();
 
+// NOTE: In react-router-redux 2.1.0, these modules fail
+// https://travis-ci.org/rackt/react-router-redux/jobs/104947473
+
+// const middleware = syncHistory(hashHistory);
+// middleware.listenForReplays(store);
+
 ReactDOM.render(
-  <Root store={store}/>,
+  <Root store={store} history={hashHistory}/>,
   document.getElementById('root')
 );
