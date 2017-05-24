529341bda448b9bd663b7e5cf6bc8c024ce17031
Fredrik Lindell <fredrik@cafebabe.se>
2017-05-24 22:26:26 +0200

    Adding Counter and Footer jsx
    * Adding Counter and Footer jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Footer = () => (
+  <footer>
+    <span id="footer-text-wrap">
+    Made in SF with <span id="heart">♥</span> by <a href="http://www.davidtsai.codes">David Tsai</a>.
+    </span>
+  </footer>
+)
+
+export default Footer;
