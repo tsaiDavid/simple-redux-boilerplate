4c364598dfa313be4d47924d2ce6c300e478ee07
Mark Sarpong <mark.sarpong@oup.com>
2017-12-09 22:38:10 +0000

    Removed deprecated prop-types

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'react'
 
 export default class Counter extends Component {
   static propTypes = {
