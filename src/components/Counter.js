73c854fe412a914dd6240a0bc70777b1ee28ede2
Mark Sarpong <mark.sarpong@oup.com>
2017-12-09 22:38:10 +0000

    Removed deprecated prop-types
    fixup! Removed deprecated prop-types

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import PropTypes from 'react'
+import PropTypes from 'prop-types'
 
 export default class Counter extends Component {
   static propTypes = {
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import PropTypes from 'react'
+import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import * as CounterActions from '../actions/CounterActions'
